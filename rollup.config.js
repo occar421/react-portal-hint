064b81731906d6ab39d3e6471a8ddfcdbfc32015
MasuqaT <occar@hotmail.co.jp>
2019-01-06 13:51:23 +0900

    chore: remove rollup-plugin-copy-glob

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from "rollup-plugin-typescript2";
-import copy from "rollup-plugin-copy-glob";
 
 export default {
   input: "src/index.ts",
@@ -10,8 +9,7 @@ export default {
   plugins: [
     typescript({
       cacheRoot: ".rts2_cache" // default value. Explicitly set for .gitignore
-    }),
-    copy([{ files: "src/*.css", dest: "dist" }])
+    })
   ],
   external: ["react", "react-dom", "resize-observer-polyfill"]
 };
