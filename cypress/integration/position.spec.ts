072d0bd4bb81e77aa86b39256170f3d8c2b3378f
MasuqaT <occar@hotmail.co.jp>
2019-02-03 20:39:41 +0900

    test: e2e test for basic place property

--- cypress/integration/position.spec.ts
+++ cypress/integration/position.spec.ts
@@ -14,30 +14,85 @@ function generateStoryUrl(
   )}&selectedStory=${encodeURIComponent(story)}`;
 }
 
+function selectPlace(place: string) {
+  cy.contains("Knobs").click();
+  cy.get(`[value="Left > Top > Right (custom fallback)"]`)
+    .parent()
+    .select(place);
+}
+
+function showHintAndCollectElement() {
+  return cy.get("#storybook-preview-iframe").then($iframe => {
+    const $body = $iframe.contents().find("body");
+
+    cy.wrap($body)
+      .contains("Toggle")
+      .click();
+
+    return cy
+      .wrap($body)
+      .find(`[data-testid="target"]`)
+      .then($target => {
+        const target = $target.get(0);
+
+        return cy
+          .wrap($body)
+          .find(".react-portal-hint__body")
+          .then($hint => {
+            const hint = $hint.get(0);
+
+            return { target, hint };
+          });
+      });
+  });
+}
+
 context("Position", () => {
-  it("top", () => {
+  beforeEach(() => {
     cy.visit(generateStoryUrl("Default", "place"));
+  });
 
-    cy.get("#storybook-preview-iframe").then($iframe => {
-      const $body = $iframe.contents().find("body");
+  it("top", () => {
+    selectPlace("Top");
 
-      cy.wrap($body)
-        .contains("Toggle")
-        .click();
+    showHintAndCollectElement().then(({ target, hint }) => {
+      const targetTop = target.getBoundingClientRect().top;
+      const hintBottom = hint.getBoundingClientRect().bottom;
 
-      cy.wrap($body)
-        .find(`[data-testid="target"]`)
-        .then($target => {
-          const targetRect = $target.get(0).getBoundingClientRect();
+      expect(hintBottom).to.be.lessThan(targetTop);
+    });
+  });
 
-          cy.wrap($body)
-            .find(".react-portal-hint__body")
-            .then($hint => {
-              const hintRect = $hint.get(0).getBoundingClientRect();
+  it("right", () => {
+    selectPlace("Right");
 
-              expect(hintRect.bottom).to.be.lessThan(targetRect.top);
-            });
-        });
+    showHintAndCollectElement().then(({ target, hint }) => {
+      const targetRight = target.getBoundingClientRect().right;
+      const hintLeft = hint.getBoundingClientRect().left;
+
+      expect(hintLeft).to.be.greaterThan(targetRight);
+    });
+  });
+
+  it("bottom", () => {
+    selectPlace("Bottom");
+
+    showHintAndCollectElement().then(({ target, hint }) => {
+      const targetBottom = target.getBoundingClientRect().bottom;
+      const hintTop = hint.getBoundingClientRect().top;
+
+      expect(hintTop).to.be.greaterThan(targetBottom);
+    });
+  });
+
+  it("left", () => {
+    selectPlace("Left");
+
+    showHintAndCollectElement().then(({ target, hint }) => {
+      const targetLeft = target.getBoundingClientRect().left;
+      const hintRight = hint.getBoundingClientRect().right;
+
+      expect(hintRight).to.be.lessThan(targetLeft);
     });
   });
 });
