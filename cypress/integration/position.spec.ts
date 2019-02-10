617965cb0d2ce2964ecccb1aee661bf06d47913b
MasuqaT <occar@hotmail.co.jp>
2019-02-10 22:33:56 +0900

    test: for place option "column" and "row"

--- cypress/integration/position.spec.ts
+++ cypress/integration/position.spec.ts
@@ -36,6 +36,16 @@ class TestHandler {
       .select(place);
   }
 
+  public dragAndDragTargetTo(x: number, y: number) {
+    cy.wrap(this.$body)
+      .contains("Move")
+      .trigger("mousedown", "center")
+      .trigger("mousemove", { clientX: x, clientY: y })
+      .trigger("mouseup", { force: true });
+
+    cy.wait(100); // considering animation delay
+  }
+
   public toggleHint() {
     cy.wrap(this.$body)
       .contains("Toggle")
@@ -117,6 +127,60 @@ context("Position", () => {
       const hintRight = hint.getBoundingClientRect().right;
 
       expect(hintRight).to.be.lessThan(targetLeft);
+    });
+  });
+
+  it("column", () => {
+    handler.selectPlaceOption("Column");
+
+    handler.dragAndDragTargetTo(250, 250);
+
+    handler.toggleHint();
+
+    handler.collectElements().then(({ target, hint }) => {
+      const targetTop = target.getBoundingClientRect().top;
+      const hintBottom = hint.getBoundingClientRect().bottom;
+
+      // if enough space exists, hint is above the target
+      expect(hintBottom).to.be.lessThan(targetTop);
+
+      handler.dragAndDragTargetTo(250, 50);
+
+      // tslint:disable-next-line:no-shadowed-variable
+      handler.collectElements().then(({ target, hint }) => {
+        const targetBottom = target.getBoundingClientRect().bottom;
+        const hintTop = hint.getBoundingClientRect().top;
+
+        // if no space, hint is below the target
+        expect(hintTop).to.be.greaterThan(targetBottom);
+      });
+    });
+  });
+
+  it("row", () => {
+    handler.selectPlaceOption("Row");
+
+    handler.dragAndDragTargetTo(250, 250);
+
+    handler.toggleHint();
+
+    handler.collectElements().then(({ target, hint }) => {
+      const targetLeft = target.getBoundingClientRect().left;
+      const hintRight = hint.getBoundingClientRect().right;
+
+      // if enough space exists, hint is on left of the target
+      expect(hintRight).to.be.lessThan(targetLeft);
+
+      handler.dragAndDragTargetTo(50, 250);
+
+      // tslint:disable-next-line:no-shadowed-variable
+      handler.collectElements().then(({ target, hint }) => {
+        const targetRight = target.getBoundingClientRect().right;
+        const hintLeft = hint.getBoundingClientRect().left;
+
+        // if no space, hint is on right of the target
+        expect(hintLeft).to.be.greaterThan(targetRight);
+      });
     });
   });
 });
