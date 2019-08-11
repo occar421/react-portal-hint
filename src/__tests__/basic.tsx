e8eb1a4aa0a67f970f1d23741d263138fe53fe95
MasuqaT <occar@hotmail.co.jp>
2019-08-11 22:39:50 +0900

    chore: use @testing-library/react instead of (old) react-testing-library

--- src/__tests__/basic.tsx
+++ src/__tests__/basic.tsx
@@ -2,7 +2,7 @@
 /* tslint:disable:no-implicit-dependencies no-submodule-imports */
 import "jest-dom/extend-expect";
 import * as React from "react";
-import { cleanup, render } from "react-testing-library";
+import { cleanup, render } from "@testing-library/react";
 import Hint from "../index";
 
 afterEach(cleanup);
